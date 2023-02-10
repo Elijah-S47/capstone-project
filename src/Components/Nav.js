9b678037f90dee7434063abf9feabcb1f73fd27a
Elijah Sydow <elijahsydow@gmail.com>
2023-02-09 20:59:08 -0700

    changes

---
+++ src/Components/Nav.js
@@ -0,0 +1,16 @@
+function Nav () {
+    return (
+        <nav>
+            <ul>
+                <li a href="">Home</li>
+                <li a href="">About</li>
+                <li a href="">Menu</li>
+                <li a href="">Reservations</li>
+                <li a href="">Order Online</li>
+                <li a href="">Login</li>
+            </ul>
+        </nav>
+    );
+};
+
+export default Nav;
