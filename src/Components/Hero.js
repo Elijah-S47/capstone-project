6010a95ec1e7ff8b5606aa33d909e31c879b5046
Elijah Sydow <elijahsydow@gmail.com>
2023-03-19 01:07:13 -0600

    Finished Nav component and almost finished Hero component

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -1,9 +1,24 @@
 import React from 'react'
-import './Hero.css';
+
+
+import { Restauranfood } from '../icons_assets'
+import { Box, Button, Heading, Image, Text, Stack, VStack } from '@chakra-ui/react'
 
 const Hero = () => {
   return (
-    <div></div>
+    <Stack bg='#495E57' display='flex' flexDirection= 'collumn' justifyContent='center' >
+      <VStack alignItems='start' alignContent='start' display='flex' sx={{ padding: { base : '20px', sm: '30px', lg: '40px' }  }} >
+        <Heading sx={{ px: { base : '10px', sm: '20px', lg: '30px' }, fontSize: { base: '20px', sm: '35px', md: '50px', lg:'80px'} }} color='#F4CE14' >Little Lemon</Heading>
+        <Heading sx={{ px: { base : '10px', sm: '20px', lg: '30px' }, fontSize: { base: '16px', sm: '30px', md: '40px', lg:'70px'} }} color='#EDEFEE' >Chicago</Heading>
+        <Text sx={{ px: { base : '10px', sm: '20px', lg: '30px' }, fontSize: { base: '5px', sm: '15px', md: '20px', lg:'30px'} }} color='#EDEFEE' >We are a family owned Mediterranean restaurant, <br /> focused on traditional <br /> recipes served with a modern <br /> twist.</Text>
+        <Box sx={{ px: { base : '10px', sm: '20px', lg: '30px' } }}  >
+          <Button bg='#F4CE14' alignSelf='start' color='#000000' sx={{ boxSize: { base: '10px' }, fontSize: { base: '4px' } }}>Reserve A Table</Button>
+        </Box>
+      </VStack>
+      <Box alignSelf='start' sx={{ padding: { base : '20px', sm: '30px', lg: '40px' }  }} >
+        <Image borderRadius='60%' objectFit='contain' sx={{ width: { base: '100px', sm: '200px', md: '300px', lg: '398px' } , px: { base : '10px', sm: '20px', lg: '30px' }  }} src={Restauranfood} alt='logo' />
+      </Box>
+    </Stack>
   )
 }
 
--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -4,19 +4,15 @@ import { Image, Link, HStack, } from '@chakra-ui/react';
 
 function Nav () {
     return (
-        <nav>
             <HStack sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center', fontFamily: 'Markazi Text', }}>
-                <Link>
-                    <Image sx={{ padding: { base : '2px', sm: '10px', lg: '40px' }, width: { base: '30px', sm: '100px', md: '100px', lg: '250px', } }} src={Logo} alt='logo' class="nav-item" />
-                </Link>
-                <Link sx={{ padding: { base : '2px', sm: '5px', lg: '10px' }, fontSize: { base: '5px', sm: '15px', md: '20px', lg:'35px'} }} class="nav-item" a href="">Home</Link>
-                <Link padding={{ base : '2px', sm: '5px', lg: '10px' }} fontSize={{ xs: '5px', sm: '15px', md: '20px', lg:'35px'}} class="nav-item" a href="">About</Link>
-                <Link padding={{ base : '2px', sm: '5px', lg: '10px' }} fontSize={{ xs: '5px', sm: '15px', md: '20px', lg:'35px'}} class="nav-item" a href="">Menu</Link>
-                <Link padding={{ base : '2px', sm: '5px', lg: '10px' }} fontSize={{ xs: '5px', sm: '15px', md: '20px', lg:'35px'}} class="nav-item" a href="">Reservations</Link>
-                <Link padding={{ base : '2px', sm: '5px', lg: '10px' }} fontSize={{ xs: '5px', sm: '15px', md: '20px', lg:'35px'}} class="nav-item" a href="">Order Online</Link>
-                <Link padding={{ base : '2px', sm: '5px', lg: '10px' }} fontSize={{ xs: '5px', sm: '15px', md: '20px', lg:'35px'}} class="nav-item" a href="">Login</Link>
+                <Image sx={{ padding: { base : '2px', sm: '10px', lg: '40px' }, width: { base: '30px', sm: '100px', md: '100px', lg: '250px', } }} src={Logo} alt='logo' />
+                <Link color="#495E57" sx={{ padding: { base : '2px', sm: '5px', lg: '10px' }, fontSize: { base: '5px', sm: '15px', md: '20px', lg:'30px'} }} a href="" >Home</Link>
+                <Link color="#495E57" sx={{ padding: { base : '2px', sm: '5px', lg: '10px' }, fontSize: { base: '5px', sm: '15px', md: '20px', lg:'30px'} }} a href="" >About</Link>
+                <Link color="#495E57" sx={{ padding: { base : '2px', sm: '5px', lg: '10px' }, fontSize: { base: '5px', sm: '15px', md: '20px', lg:'30px'} }} a href="">Menu</Link>
+                <Link color="#495E57" sx={{ padding: { base : '2px', sm: '5px', lg: '10px' }, fontSize: { base: '5px', sm: '15px', md: '20px', lg:'30px'} }} >Reservations</Link>
+                <Link color="#495E57" sx={{ padding: { base : '2px', sm: '5px', lg: '10px' }, fontSize: { base: '5px', sm: '15px', md: '20px', lg:'30px'} }} >Order Online</Link>
+                <Link color="#495E57" sx={{ padding: { base : '2px', sm: '5px', lg: '10px' }, fontSize: { base: '5px', sm: '15px', md: '20px', lg:'30px'} }} >Login</Link>
             </HStack>
-        </nav>
     );
 };
 
