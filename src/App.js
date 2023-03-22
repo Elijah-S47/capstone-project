b001f55b26d5f4da8e506b15b1c0d47a41e871f2
Elijah Sydow <elijahsydow@gmail.com>
2023-03-21 21:54:43 -0600

    Publishing

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import React from 'react'
 
 import { AboutPage, HomePage, MenuPage, ReservationsPage } from "./pages";
 import "./App.css";
