4555b8d968d4be4ca5abed0a0263657b0fe8d63d
Elijah Sydow <elijahsydow@gmail.com>
2023-03-21 22:58:24 -0600

    Publishing

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -7,12 +7,12 @@ function Nav () {
             <HStack
             sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center', fontFamily: 'Markazi Text', }}>
                 <Image sx={{ padding: { base : '2px', sm: '10px', lg: '40px' }, width: { base: '60px', sm: '100px', md: '100px', lg: '250px', } }} src={Logo} alt='logo' />
-                <Link color="#495E57" sx={{ padding: { base : '2px', sm: '5px', lg: '10px' }, fontSize: { base: '8px', sm: '15px', md: '20px', lg:'30px'} }} a href="/homepage" >Home</Link>
-                <Link color="#495E57" sx={{ padding: { base : '2px', sm: '5px', lg: '10px' }, fontSize: { base: '8px', sm: '15px', md: '20px', lg:'30px'} }} a href="/aboutpage" >About</Link>
-                <Link color="#495E57" sx={{ padding: { base : '2px', sm: '5px', lg: '10px' }, fontSize: { base: '8px', sm: '15px', md: '20px', lg:'30px'} }} a href="/menupage">Menu</Link>
-                <Link color="#495E57" sx={{ padding: { base : '2px', sm: '5px', lg: '10px' }, fontSize: { base: '8px', sm: '15px', md: '20px', lg:'30px'} }} a href="/reservationspage" >Reservations</Link>
-                <Link color="#495E57" sx={{ padding: { base : '2px', sm: '5px', lg: '10px' }, fontSize: { base: '8px', sm: '15px', md: '20px', lg:'30px'} }} a href="/menupage" >Order Online</Link>
-                <Link color="#495E57" sx={{ padding: { base : '2px', sm: '5px', lg: '10px' }, fontSize: { base: '8px', sm: '15px', md: '20px', lg:'30px'} }} a href="/homepage" >Login</Link>
+                <Link color="#495E57" sx={{ padding: { base : '2px', sm: '5px', lg: '10px' }, fontSize: { base: '8px', sm: '15px', md: '20px', lg:'30px'} }} a href="/capstone-project/homepage" >Home</Link>
+                <Link color="#495E57" sx={{ padding: { base : '2px', sm: '5px', lg: '10px' }, fontSize: { base: '8px', sm: '15px', md: '20px', lg:'30px'} }} a href="/capstone-project/aboutpage" >About</Link>
+                <Link color="#495E57" sx={{ padding: { base : '2px', sm: '5px', lg: '10px' }, fontSize: { base: '8px', sm: '15px', md: '20px', lg:'30px'} }} a href="/capstone-project/menupage">Menu</Link>
+                <Link color="#495E57" sx={{ padding: { base : '2px', sm: '5px', lg: '10px' }, fontSize: { base: '8px', sm: '15px', md: '20px', lg:'30px'} }} a href="/capstone-project/reservationspage" >Reservations</Link>
+                <Link color="#495E57" sx={{ padding: { base : '2px', sm: '5px', lg: '10px' }, fontSize: { base: '8px', sm: '15px', md: '20px', lg:'30px'} }} a href="/capstone-project/menupage" >Order Online</Link>
+                <Link color="#495E57" sx={{ padding: { base : '2px', sm: '5px', lg: '10px' }, fontSize: { base: '8px', sm: '15px', md: '20px', lg:'30px'} }} a href="/capstone-project/homepage" >Login</Link>
             </HStack>
     );
 };
