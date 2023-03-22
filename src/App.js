136f60880d83f74e78866d7b157b61113572d8aa
Elijah Sydow <elijahsydow@gmail.com>
2023-03-21 23:14:15 -0600

    changes

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import React from 'react'
 
-import { AboutPage, HomePage, MenuPage, ReservationsPage } from "./pages";
+import { LandingPage, AboutPage, HomePage, MenuPage, ReservationsPage } from "./pages";
 import "./App.css";
 
 function App() {
   return (
-    <Router>
-      <Routes basename="/capstone-project">
-        <Route exact path="/capstone-project" element={<HomePage />} />
+      <Routes>
+        <Route exact path="/capstone-project" element={<LandingPage />} />
         <Route exact path="/capstone-project/homepage" element={<HomePage />} />
         <Route exact path="/capstone-project/aboutpage" element={<AboutPage />} />
         <Route exact path="/capstone-project/menupage" element={<MenuPage />} />
         <Route exact path="/capstone-project/reservationspage" element={<ReservationsPage />} />
       </Routes>
-    </Router>
   );
 }
 
