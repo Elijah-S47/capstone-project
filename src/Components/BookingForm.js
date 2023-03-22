87cc02b0510b36bf8d30ac39d15c891d00d08f52
Elijah Sydow <elijahsydow@gmail.com>
2023-03-21 21:08:19 -0600

    semantics

--- src/Components/BookingForm.js
+++ src/Components/BookingForm.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import { useState } from 'react'
 
 import { Box, Input, FormControl, FormLabel, Select } from '@chakra-ui/react'
 import { Greeksalad } from '../icons_assets'
