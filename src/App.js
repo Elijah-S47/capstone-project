e401f4387c7f7f537c3de09c5e4fb2aa4f97b155
Elijah Sydow <elijahsydow@gmail.com>
2023-03-21 22:40:59 -0600

    Publishing

--- src/App.js
+++ src/App.js
@@ -7,11 +7,12 @@ import "./App.css";
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route exact path="/Elijah-S47.github.io/capstone-project/" element={<HomePage />} />
-        <Route exact path="/Elijah-S47.github.io/capstone-project/aboutpage" element={<AboutPage />} />
-        <Route exact path="/Elijah-S47.github.io/capstone-project/menupage" element={<MenuPage />} />
-        <Route exact path="/Elijah-S47.github.io/capstone-project/reservationspage" element={<ReservationsPage />} />
+      <Routes basename="/capstone-project">
+        <Route exact path="/capstone-project" element={<HomePage />} />
+        <Route exact path="/capstone-project/homepage" element={<HomePage />} />
+        <Route exact path="/capstone-project/aboutpage" element={<AboutPage />} />
+        <Route exact path="/capstone-project/menupage" element={<MenuPage />} />
+        <Route exact path="/capstone-project/reservationspage" element={<ReservationsPage />} />
       </Routes>
     </Router>
   );
